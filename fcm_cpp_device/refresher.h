1dcc404ff36c504abeda51ef34ebd3d77604dfbf
Katherine Zamudio-Turcotte <kathy_zt@yahoo.ca>
2023-03-04 18:55:40 -0500

    done timer start in refresher

---
+++ fcm_cpp_device/refresher.h
@@ -0,0 +1,15 @@
+#include "plane.h"
+#include "a429_lib.h"
+
+class Refresher {
+  public:
+    Refresher(Plane* plane, A429* sender);
+    ~Refresher();
+  private:
+    void   UpdatePlane(unsigned long refreshRate);
+    void   StartPeriodicTimer();
+    void   TimerThread(union sigval arg);
+    Plane* planeState;
+    A429*  sender;
+    static const unsigned long INTERVAL_NS;
+};
